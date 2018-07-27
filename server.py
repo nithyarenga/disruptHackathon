55a60885eb1f4cb76802708b0ba7bf055b680465
Nithya Renganathan <narengan@us.ibm.com>
2018-07-26 22:44:04 -0500

    reco

--- server.py
+++ server.py
@@ -5,10 +5,12 @@ import requests
 from json import dumps, loads
 import json
 import pusher
+import threading
+import time
 
 
 group_id=""
-user_id=""
+user_id="siddhukrs"
 
 @get('/hello')
 def hello():
@@ -32,11 +34,14 @@ def token():
     instagram_user_urls = read_instagram_feed(v['user_id'],v['access_token'])
     group_id = v['group_id']
     response.status = 200
-
+    #print  instagram_user_urls
+    url_list = {"key":instagram_user_urls}
+    thr = threading.Thread(target=gopis_method, args=(url_list), kwargs={})
     retry =  gopis_method(instagram_user_urls)
-    return {"result": retry}
+    thr.start()
+    return {"result": 200}
 
-def pusher(urls):
+def push_sid(urls):
     pusher_client = pusher.Pusher(
         app_id='562354',
         key='472deb41d62feac32b9b',
@@ -63,17 +68,17 @@ def read_instagram_feed(user_id,access_token):
 
 def gopis_method(instagram_urls):
     result = [{
-        "image": "https://scontent.cdninstagram.com/vp/87c04d0814eb22217015f5d4757c5aeb/5C0D0AF2/t51.2885-15/sh0.08/e35/s640x640/37254209_2119495828322471_6795489576329674752_n.jpg?_nc_eui2=AeGwhmWbrK2upocXw-fitQ_XtU8urjdHJuMnzfaBnVJtH1BjmaHwo-8MG0W6gYuTM0xjfzRLQYNhSdmC_aJbxIbv",
+        "url": "https://scontent.cdninstagram.com/vp/87c04d0814eb22217015f5d4757c5aeb/5C0D0AF2/t51.2885-15/sh0.08/e35/s640x640/37254209_2119495828322471_6795489576329674752_n.jpg?_nc_eui2=AeGwhmWbrK2upocXw-fitQ_XtU8urjdHJuMnzfaBnVJtH1BjmaHwo-8MG0W6gYuTM0xjfzRLQYNhSdmC_aJbxIbv",
         "location": "Austin",
-        "tags":["adventure" , "music" , "food"]
+        "description":["adventure" , "music" , "food"]
     },
     {
         "image": "https://scontent.cdninstagram.com/vp/9fb93cba53ad14819ff20b4044434721/5BF21B8A/t51.2885-15/sh0.08/e35/s640x640/36160470_1036318143194877_7432221918230478848_n.jpg?_nc_eui2=AeFuns6crQY85jwZFDPKqZai8z76-sZPK60lxUaYIpb-XENHpCHB2knRX6uxb7GnN-skGIm_o-ZcMT5tL-IQG4-I",
-        "location": "Austin",
-        "tags":["adventure" , "music" , "food"]
+        "location": "Germany",
+        "description":["hills" , "music" , "culture"]
     }
     ]
-    return result
+    push_sid(result)
 
 
 run(host='localhost', port=8080, debug=True)
